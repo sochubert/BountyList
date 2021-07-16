82de3d09c64389d0290a99a54cf0c04d56d11952
Minjae Lee <sochubert@gmail.com>
2021-07-16 14:25:30 +0900

    init

---
+++ BountyList/BountyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BountyViewController.swift
+//  BountyList
+//
+//  Created by 무빙키 on 2021/07/13.
+//
+
+import UIKit
+
+class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // UITableVIewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+}
